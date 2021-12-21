d16530f06070da7ff89290f670209faa7a6af653
khs20137 <khs20137@naver.com>
2021-12-21 23:30:01 +0900

    챗봇 데이터 생성 시작

--- data/make_data.py
+++ data/make_data.py
@@ -1,1 +1,12 @@
 # make_data.py
+import re
+
+
+script = open('How_I_Met_Your_Mother_S01_EP01.txt', 'r', encoding='utf-8')
+
+for row in script.readlines():
+    # print(row.strip())
+    if len(row.strip().split(':')) >= 2:
+        print(row.strip().split(':'))
+
+# 마셜을 먼저 만들어 보자 마셜과 다른 사람이 대화하는 것을 따로 빼오는 코드 생각해보기
