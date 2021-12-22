0f55129a8e2ab46242e09263a4028f21fb580a2d
khs20137 <khs20137@naver.com>
2021-12-22 23:20:25 +0900

    Marshall과의 대화 추출

--- data/make_data.py
+++ data/make_data.py
@@ -4,9 +4,16 @@ import re
 
 script = open('How_I_Met_Your_Mother_S01_EP01.txt', 'r', encoding='utf-8')
 
+speaking = []
 for row in script.readlines():
     # print(row.strip())
     if len(row.strip().split(':')) >= 2:
-        print(row.strip().split(':'))
+        speaking.append(row.strip().split(':'))
+
+for n in range(len(speaking)):
+    if speaking[n][0] == 'Marshall':
+        print(speaking[n-1])
+        print(speaking[n])
+        # print(speaking[n+1])
 
 # 마셜을 먼저 만들어 보자 마셜과 다른 사람이 대화하는 것을 따로 빼오는 코드 생각해보기
